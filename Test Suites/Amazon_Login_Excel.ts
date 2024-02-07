557fb246a4617f7164221eb3fe87d435b35375de
asmishri2004 <103985485+asmishri2004@users.noreply.github.com>
2024-02-07 22:44:13 +0530

    Add files via upload

---
+++ Test Suites/Amazon_Login_Excel.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon_Login_Excel</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3a40bd64-d641-4989-a827-60539efaef21</testSuiteGuid>
+   <testCaseLink>
+      <guid>1cfd7fb6-b8cb-4006-87f3-3eb9f423f609</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Amazon/TC_Amazon_Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>4e784b31-454f-445d-ba39-36a7fb835efa</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Amazon_Login_Excel/Amazon_Excel_Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>3c3955c3-c5ab-41f6-bdcd-063c6a318443</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>4e784b31-454f-445d-ba39-36a7fb835efa</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>e3d589da-655c-4c22-9725-00655101d706</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
